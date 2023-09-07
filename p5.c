13490ab67a01a0b1ba32d6fb0e026af2a0e01a0d
JaiKumar092 <142215489+JaiKumar092@users.noreply.github.com>
2023-09-07 21:33:32 +0530

    Update p5.c

--- p5.c
+++ p5.c
@@ -1,306 +1,187 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
+typedef struct NODE{
+	char station;
+	struct NODE* next;
+} node;
 
-typedef struct mynode {
-	char name[20];
-	char gen[6];
-	int age;
-	struct mynode* link;
-} Node;
-
-Node* start = NULL;
-
-void details(int);
-int seat(int);
-int cal(int, int, int);
-void bill(int, int);
-
-
-char source[20], des[20], train[40];
-char station[40], cla[40];
-int time1, time2, a[55];
-
-
-void main()
-{
-        int x = 0;
-	int i, j, a1, a2, b, c, d, e, r;
-	char o;
-	printf("Enter Number Of Passengers: ");
-	fflush(stdin);
-	scanf("%d", &j);
-
-	
-	details(j);
-	printf("Enter The Source Place: ");
-	fflush(stdin);
-	gets(source);
-	printf("Enter The Destination Place: ");
-	gets(des);
-	printf("\t\tThe Following Trains "
-		"Are Available.....\n");
-	printf("\t\t1. Rajdhani Express.."
-		".......10:00 "
-		"a.m........Sealdah Station\n");
-	printf("\t\t2. Satabdi Express..."
-		".......05:00 "
-		"p.m........Howrah Station\n");
-	printf("\t\t3. Humsafar Express..."
-		".......11:00 "
-		"p.m........Kolkata Chitpur"
-		" Station\n");
-	printf("\t\t4. Garib-Rath Express"
-		".........05:00 "
-		"p.m........Sealdah Station\n");
-	printf("\t\t5. Duronto Express..."
-		".........07:00 "
-		"a.m.........Santraganchi"
-		"Station\n");
-	scanf("%d", &i);
-	do {
-		switch (i) {
-		case 1: {
-			strcpy(train,
-				"Rajdhani Express");
-			strcpy(station,
-				"Sealdah Station");
-			time1 = 10;
-			time2 = 00;
-			a1 = 2099;
-			a2 = 1560;
-
-			
-			d = cal(a1, a2, j);
-			printf("Total Bill Amount:"
-				" %d\n",
-				d);
-		}; break;
-		case 2: {
-			strcpy(train,
-				"Satabdi Express");
-			strcpy(station,
-				"Howrah Station");
-			time1 = 05;
-			time2 = 00;
-			a1 = 1801;
-			a2 = 981;
-
-			
-			d = cal(a1, a2, j);
-			printf("Total Bill Amount:"
-				"%d\n",
-				d);
-		}; break;
-		case 3: {
-			strcpy(train,
-				"Humsafar Express");
-			strcpy(station,
-				"Kolkata Chitpur Express");
-			time1 = 11;
-			time2 = 00;
-			a1 = 2199;
-			a2 = 1780;
-
-			d = cal(a1, a2, j);
-			printf("Total Bill Amount: %d\n", d);
-		}; break;
-		case 4: {
-			strcpy(train, "Garib-Rath Express");
-			strcpy(station, "Sealdah Station");
-			time1 = 05;
-			time2 = 00;
-			a1 = 1759;
-			a2 = 1200;
-
-			
-			d = cal(a1, a2, j);
-			printf("Total Bill Amount: %d\n", d);
-		}; break;
-		case 5: {
-			strcpy(train, "Duronto Express");
-			strcpy(station, "Santraganchi Station");
-			time1 = 07;
-			time2 = 00;
-			a1 = 2205;
-			a2 = 1905;
-
-			
-			d = cal(a1, a2, j);
-			printf("Total Bill Amount: %d\n", d);
-		}; break;
-		default:
-			printf("Enter Correct choice.....\n");
-			x = 1;
-			break;
-		}
-	} while (x);
-	printf("Now Book Your Seats......\n");
-
-	
-	seat(j);
-
-	
-	bill(d, j);
+node* createNode(char c){
+	node* temp = (node*)malloc(sizeof(node));
+	temp->station = c;
+	temp->next = NULL;
+	return temp;
 }
 
-
-int cal(int y1, int y2, int h)
-{
-	int b, c, i, t, r, n;
-	printf("\t\tEnter Your Choice......\n");
-	printf("\t\t1. Sleeper Class....\n");
-	printf("\t\t2. A.C Class.......\n");
-	scanf("%d", &i);
-	switch (i) {
-	case 1: {
-		strcpy(cla, "Sleeper Class");
-		b = y2 * h;
-		c = b + (b * 0.18);
-	} break;
-	case 2: {
-		printf("\t\tEnter Your Choice....\n");
-		printf("\t\t1. 3A Class....\n");
-		printf("\t\t2. 2A Class....\n");
-		printf("\t\t3. 1st Class A.C.....\n");
-		scanf("%d", &n);
-		switch (n) {
-		case 1: {
-			strcpy(cla, "3A Class");
-			b = y1 * h;
-			c = b + (b * 0.18);
-		} break;
-		case 2: {
-			strcpy(cla, "2A Class");
-			b = (y1 + 1000) * h;
-			c = b + (b * 0.18);
-		} break;
-		case 3: {
-			strcpy(cla, "1st Class A.C.");
-			b = (y1 + 5000) * h;
-			c = b + (b * 0.18);
-		} break;
-		default: {
-			printf("\t\tEnter Right Choice......\n");
-		}
-		}
-	} break;
-	default: {
-		printf("\t\tEnter Right Choice......\n");
+node* addNode(node* head, char c){
+	node* temp = head;
+	if(head == NULL){
+		return createNode(c);
 	}
+	while(head->next != NULL){
+		head = head->next;
 	}
-	return c;
+	head->next = createNode(c);
+	return temp;
 }
 
-
-void details(int k)
-{
-	int i, a;
-	char val[20], gen[6];
-	for (i = 1; i <= k; i++) {
-		printf("Enter The %dth Passenger Name: ", i);
-		fflush(stdin);
-		gets(val);
-		printf("Enter The %dth Passenger Gender: ", i);
-		fflush(stdin);
-		gets(gen);
-		printf("Enter The %dth Passenger Age: ", i);
-		fflush(stdin);
-		scanf("%d", &a);
-
-		
-		add_node(val, gen, a);
-	}
-}
-
-
-void add_node(char lol[20], char der[6], int b)
-{
-	Node *newptr = NULL, *ptr;
-	newptr = (Node*)malloc(sizeof(Node));
-	strcpy(newptr->name, lol);
-	strcpy(newptr->gen, der);
-	newptr->age = b;
-	newptr->link = NULL;
-	if (start == NULL)
-		start = newptr;
-	else {
-		ptr = start;
-		while (ptr->link != NULL)
-			ptr = ptr->link;
-		ptr->link = newptr;
-	}
-}
-
-
-int seat(int p)
-{
-	int i;
-	printf("\t		 -:SEAT MATRIX:-	 \n");
-	printf("\t(U) (M)	 (L) (L) "
-		" (U)\n\n");
-	printf("\t01 02	 03\t04	 "
-		"05\n\n");
-	printf("\t06 07	 08\t09	 "
-		"10\n");
-	printf("\t11 12	 13\t14	 "
-		"15\n\n");
-	printf("\t16 17	 18\t19	 "
-		"20\n");
-	printf("\t21 22	 23\t24	 "
-		"25\n\n");
-	printf("\t26 27	 28\t29	 "
-		"30\n");
-	printf("\t31 32	 33\t34	 "
-		"35\n\n");
-	printf("\t36 37	 38\t39	 "
-		"40\n");
-	printf("\t41 42	 43\t44	 "
-		"45\n\n");
-	printf("\t46 47	 48\t49	 "
-		"50\n");
-	printf("\t51 52	 53\t54	 "
-		"55\n\n");
-	printf("\t56 57	 58\t59	 "
-		"60\n");
-	printf("\tEnter Seat Numbers: \n");
-	for (i = 0; i < p; i++)
-		scanf("%d", &a[i]);
-}
-
-
-void bill(int y, int j)
-{
-	int i;
-	Node* ptr = start;
-	for (i = 1; i <= j; i++) {
-		printf("\t\t\%dst Passenger Name: ", i);
-		puts(ptr->name);
-		printf("\t\t%dst Passenger Gender: ", i);
-		puts(ptr->gen);
-		printf("\t\t%dst Passenger Age: %d\n\n", i,
-			ptr->age);
-		ptr = ptr->link;
-	}
-	printf("\t\tSource Place: ");
-	puts(source);
-	printf("\t\tDestination Place: ");
-	puts(des);
-	printf("\t\tThe Boarding Station: ");
-	puts(station);
-	printf("\t\tTrain Is: ");
-	puts(train);
-	printf("\t\tAllocated Class: ");
-	puts(cla);
-	printf("\t\tBoarding Time: %d:%d\n", time1, time2);
-	printf("\t\tTotal Bill Amount: %d\n", y);
-	printf("\t\tAllocated Seats Are: \n");
-	for (i = 0; i < j; i++) {
-		printf("\t\t%d ", a[i]);
+void printList(node* head){
+	while(head != NULL){
+		printf("%c ", head->station);
+		head = head->next;
 	}
 	printf("\n");
-	printf("\t\t\t\tThank You......\n");
 }
 
+typedef struct Train
+{
+	int seats;
+	int basePrice;
+	int trainNo;
+	node* route;
+} train;
+
+train *createTrain(int tn, int prs, int seats,char routes[], int routeSize)
+{
+	train *temp = (train *)malloc(sizeof(train));
+	temp->trainNo = tn;
+	temp->basePrice = prs;
+	temp->seats = seats;
+	temp->route = NULL;
+	for (int i = 0; i < routeSize; i++)
+	{
+		temp->route = addNode(temp->route, routes[i]);
+	}	
+	return temp;
+}
+
+void bookTickets(train *tra, int n)
+{
+	(tra->seats) -= n;
+	printf("Total fare = %d\n", n * tra->basePrice);
+	printf("Ticket BOOKED\n");
+}
+
+void printTrainDetails(train* gadi){
+	if(gadi == NULL){
+		return;
+	}
+	printf("%d\t%d\t%d\n",gadi->trainNo, gadi->seats, gadi->basePrice);
+	
+}
+
+void getTrain(char src, char dest, train* gadi[], int n){
+	int trainAvail[n];
+	for (int i = 0; i < n; i++)
+	{
+		trainAvail[i] = 0;
+	}
+	
+	for (int i = 0; i < n; i++)
+	{
+		node* temp = gadi[i]->route;
+		while(temp != NULL){
+			if(temp->station == src){
+				while(temp != NULL){
+					if(temp->station == dest){
+						trainAvail[i] = 1;
+						break;
+					}
+					temp = temp->next;
+				}
+				break;
+			}
+			temp = temp->next;
+		}
+	}
+	printf("Available Trains\n");
+	printf("TrainNo.\tFare\t\tSeats\n");
+	printf("------------------------------------\n");
+	for (int i = 0; i < n; i++)
+	{
+		if(trainAvail[i]){
+			printf("%-15d\t%-15d\t%-15d\n",gadi[i]->trainNo, gadi[i]->basePrice,gadi[i]->seats);
+		}
+	}	
+}
+
+int getTrainByN(train* gadi[], int n, int trainNo){
+	for (int i = 0; i < n; i++)
+	{
+		if(gadi[i]->trainNo == trainNo){
+			return i;
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+	FILE *inp = fopen("./reservation/trainData.txt", "r");
+	if (inp == NULL)
+	{
+		printf("File does not exist\n");
+		return 1;
+	}
+	char temp[12];
+	int n;
+	fscanf(inp,"%d",&n);
+	train* gadi[n];
+	for (int i = 0; i < n; i++)
+	{
+		int tn, prs, seat;
+		fscanf(inp, "%d %d %d",&tn,&seat,&prs);
+		int routeSize;
+		fscanf(inp, "%d", &routeSize);
+		char routes[routeSize];
+		for (int i = 0; i < routeSize; i++)
+		{
+			routes[i] = getc(inp);
+		}
+		gadi[i] = createTrain(tn,prs,seat,routes,routeSize);
+	}
+	fclose(inp);
+	int t=1,ch;
+	while(t){
+		printf("Choose:\n1)Find trains\n2)Book Ticket\n3)Exit\n: ");
+		scanf("%d", &ch);
+		char src,dest;
+		int tnu;
+		int trainIndex;
+		switch (ch)
+		{
+		case 1:
+			printf("Enter src : ");
+			scanf(" %c", &src);
+			printf("Enter dest : ");
+			scanf(" %c", &dest);
+			getTrain(src,dest,gadi,n);
+			break;
+		case 2:
+			printf("Enter train no. : ");
+			scanf("%d",&ch);
+			trainIndex = getTrainByN(gadi,n,ch);
+			if(trainIndex == -1)
+			{
+				printf("Invalid train no.\n");
+				break;
+			}
+			printf("Enter number of tickets : ");
+			scanf("%d", &tnu);
+			if(gadi[trainIndex]->seats < tnu){
+				printf("Not enough seats\n");
+				break;
+			}
+			bookTickets(gadi[trainIndex],tnu);
+			break;
+		case 3:
+			t=0;
+			break;
+		default:
+			printf("Invalid Option\n");
+			break;
+		}
+	}		
+
+	return 0;
+}
